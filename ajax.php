7f637f150250eabf92e5f3287de75a41b3ebee2e
Tvisher <tvisher@mail.ru>
2022-08-16 15:48:32 +0600

    first commit

---
+++ ajax.php
@@ -0,0 +1,274 @@
+<li class="list-item" data-type-postcard="proactivity">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/proactivity.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за проактивность</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="high-quality">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/high-quality.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо<br> за высокое качество</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="great-dedication">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/great-dedication.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за большую самоотдачу</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="brilliant-ideas">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/brilliant-ideas.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за блестящие идеи</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="reliable-shoulder">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/reliable-shoulder.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо<br> за надежное плечо</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="understanding-and-support">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/understanding-and-support.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за понимание <br>и поддержку</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="attention-details">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/attention-details.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за внимание <br>к деталям</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="positive-attitude">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/positive-attitude.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за позитивный <br>настрой</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="tireless-enthusiasm">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/tireless-enthusiasm.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за неутомимый <br>энтузиазм</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="inexhaustible-energy">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/inexhaustible-energy.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо за неиссякаемую <br>энергию</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="list-item" data-type-postcard="other-thanks">
+                        <div class="list-item__wrapper">
+                            <div class="list-item__bg-image">
+                                <img src="./resources/images/thatks-proj/other-thanks.svg" alt="">
+                            </div>
+                            <div class="list-item__content">
+                                <span class="list-item__title">Спасибо</span>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">От кого:</span>
+                                    <span class="block__name">Юлия Рогова</span>
+                                    <span class="block__from">г.Москва специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__block">
+                                    <span class="block__nameplate">Кому:</span>
+                                    <span class="block__name">Ольга Литвиненко</span>
+                                    <span class="block__from">г.Саратов специалист по всем специальностям</span>
+                                </div>
+
+                                <div class="list-item__text">Благодарю от всей души за понимание, за добросердечность, отзывчивость, искренность, поддержку! Приятно знать, что такие люди есть рядом. Желаю процветания, добра, благополучия, гармонии и счастья! Искренне, от всего сердца, благодарю за помощь, своевременную и такую необходимую.</div>
+                            </div>
+                        </div>
+                    </li>
